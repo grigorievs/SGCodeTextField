8b0f2916eea9eb739a12da095068b614273c0c2e
Blazej SLEBODA <contact@blazejsleboda.fr>
2019-09-11 17:33:57 +0200

    count property should be 0 or higher

--- SGCodeTextField/SGCodeTextField.swift
+++ SGCodeTextField/SGCodeTextField.swift
@@ -31,6 +31,7 @@ public class SGCodeTextField: UIControl {
     
     @IBInspectable public var count: Int = 6 {
         didSet {
+            if count < 0 { count = 0}
             refreshUI()
         }
     }
