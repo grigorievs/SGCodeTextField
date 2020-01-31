913b755cc3396f92fc6d829fe32ae8b087cdc14f
Deryck Tomiuc <tomiuc.deryck@gmail.com>
2020-01-31 11:39:59 +0200

    Added support for OTC.

--- SGCodeTextField/SGCodeTextField.swift
+++ SGCodeTextField/SGCodeTextField.swift
@@ -193,6 +193,11 @@ public class SGCodeTextField: UIControl {
 		self.textField.keyboardType = .numberPad
 		self.textField.isHidden = true
 		self.textField.delegate = self
+        
+        /// Note: Received message must containt one of the following words for the auto-complete to work: passcode, code
+        if #available(iOS 12, *) {
+            self.textField.textContentType = .oneTimeCode
+        }
 	}
 	
 	private func addGestures() {
